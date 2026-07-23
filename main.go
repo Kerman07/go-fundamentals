17032f0763e609bbaadd6befe951b1405f11bfbc
Kerim Mandzo <kerman07@live.com>
2026-07-23 22:39:25 +0200

    done

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
-import ( 
-	"bufio"
+import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	name := scanner.Text()
-	scanner.Scan()
-	age := scanner.Text()
-	fmt.Printf("Hi, %s! You are %s years old.", name, age)
+	var integer int
+	fmt.Scan(&integer)
+
+	switch {
+	case integer%15 == 0:
+		fmt.Println("FizzBuzz")
+	case integer%5 == 0:
+		fmt.Println("Buzz")
+	case integer%3 == 0:
+		fmt.Println("Fizz")
+	default:
+		fmt.Println(integer)
+	}
 }
